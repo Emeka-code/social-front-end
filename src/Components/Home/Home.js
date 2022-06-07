4910c7edb3c306df9b37cb4d2fd24f27bc385864
Emeka-code <clintonekegbu@gmail.com>
2022-06-07 17:33:41 +0100

    update

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,6 +3,10 @@ import Header from "../Header/Header";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeScreen from "../HomeScreen/HomeScreen";
 import DetailScreen from "../Profile/DetailScreen";
+import Register from "../Auth/Register";
+import SignIn from "../Auth/Signin";
+import ResetPassword from "../Auth/ResetPassword";
+import NewPassword from "../Auth/NewPassword";
 
 const Home = () => {
   return (
@@ -11,6 +15,10 @@ const Home = () => {
       <Routes>
         <Route path="/" element={<HomeScreen />} />
         <Route path="/DetailScreen" element={<DetailScreen />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/requestReset" element={<ResetPassword />} />
+        <Route path="/newPassword" element={<NewPassword />} />
       </Routes>
     </BrowserRouter>
   );
